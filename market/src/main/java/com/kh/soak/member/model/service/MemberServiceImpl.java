c905b03168a57cc209281bbcf834fa4ab85e3c12
CJH667 <honey12020@gmail.com>
2025-05-31 14:37:58 +0900

    건든 건 딱히 없음 pull하기 전 commit

--- market/src/main/java/com/kh/soak/member/model/service/MemberServiceImpl.java
+++ market/src/main/java/com/kh/soak/member/model/service/MemberServiceImpl.java
@@ -16,34 +16,36 @@ public class MemberServiceImpl implements MemberService{
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;  //추가 수정필요  
-	//스프링 컨테이너가 sqlSession의 생명주기도 관리하기 때문에 직접 close할 필요 없고
-	//트랜잭션 처리 또한 스프링이 직접 처리하기 때문에 생략한다.
-	//단 트랜잭션 처리를 직접해야되는 경우엔 해당 메소드에 
-	//@Transactional 어노테이션을 부여하고 직접 관리하면 된다.
+	
+	
+	//로그인기능
 	@Override
 	public Member loginMember(Member m) {
 		Member loginUser = dao.loginMember(sqlSession,m);
 		return loginUser;
 	}
-	
+	//회원가입 기능
 	@Override
 	public int insertMember(Member m) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	//회원정보수정 기능
 	@Override
 	public int updateMember(Member m) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	//회원탈퇴 기능
 	@Override
 	public int deleteMember(Member m) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	//아이디 중복확인 기능
 	@Override
 	public int idCheck(String userId) {
 		// TODO Auto-generated method stub
